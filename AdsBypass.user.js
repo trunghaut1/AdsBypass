b486cfdb9716769b97a7a27af36c28efa84d21db
Trần Trung Hậu <trunghau.t1@gmail.com>
2021-09-02 12:44:01 +0700

    Update AdsBypass.user.js

--- AdsBypass.user.js
+++ AdsBypass.user.js
@@ -1,13 +1,13 @@
 // ==UserScript==
 // @name         AdsBypass
 // @namespace    https://github.com/trunghaut1/AdsBypass
-// @version      0.1
+// @version      0.2
 // @description  Skip ads countdown for some websites
 // @author       trunghaut1
 // @match        https://1link.vip/*
-// @icon         https://raw.githubusercontent.com/trunghaut1/AdsBypass/main/logo.png?token=ADNKM4ZBWTYWL3M7WUD75ILBGBRK4
+// @icon         https://raw.githubusercontent.com/trunghaut1/AdsBypass/main/logo.png
 // @homepage     https://github.com/trunghaut1/AdsBypass
-// @updateURL    https://github.com/trunghaut1/AdsBypass
+// @updateURL    https://github.com/trunghaut1/AdsBypass/raw/main/AdsBypass.user.js
 // @grant        none
 // @encoding     utf-8
 // ==/UserScript==
