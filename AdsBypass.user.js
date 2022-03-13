0b7e1d8584bdfbc20e0b2c1f433f9700ae0bf5a3
Trung Hậu <trunghau.t1@gmail.com>
2022-03-13 19:43:45 +0700

    Update

--- AdsBypass.user.js
+++ AdsBypass.user.js
@@ -1,24 +1,32 @@
 // ==UserScript==
 // @name         AdsBypass
 // @namespace    https://github.com/trunghaut1/AdsBypass
-// @version      0.3
+// @version      0.4
 // @description  Skip ads countdown for some websites
 // @author       trunghaut1
-// @match        https://1link.vip/*
 // @icon         https://raw.githubusercontent.com/trunghaut1/AdsBypass/main/logo.png
 // @homepage     https://github.com/trunghaut1/AdsBypass
 // @updateURL    https://github.com/trunghaut1/AdsBypass/raw/main/AdsBypass.user.js
 // @grant        none
 // @encoding     utf-8
+// @include https://*/*
 // ==/UserScript==
 
 (function() {
     'use strict';
-    setTimeout(function () {
-        var link = document.getElementsByClassName("btn btn-success btn-lg get-link")[0];
-        if(link.getAttribute("data-url") != null)
+    var check1s = document.getElementsByClassName("site-logo")[0].href;
+    if (check1s == 'https://link1s.com/')
+    {
+        if(document.forms[0] === undefined)
         {
-            window.location.href = link.getAttribute("data-url");
+            if(document.getElementById("link1s") != null)
+                document.getElementById("link1s").click();
+            else
+              link1sgo();
         }
-    }, 2000);
+        else
+        {
+            document.forms[0].submit();
+        }
+    }
 })();
